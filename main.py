db026a0939112129ccf0c916c08e8aca05277a84
Gabriella Conde <condeg@ufl.edu>
2023-03-21 00:30:54 -0400

    Add decode function

--- main.py
+++ main.py
@@ -11,8 +11,19 @@ def encode(password):
     # return password
     return new_password
 
+
 # decodes a password
-# def decode(new_password):
+def decode(new_password):
+    # create empty string to store decoded password
+    decoded_string = ""
+
+    # iterate through each digit in the encoded string
+    for digit in new_password:
+        # decode each digit and add to decoded string
+        decoded_string += str((int(digit) + 7) % 10)
+
+    # return the decoded password
+    return decoded_string
 
 
 def main():
@@ -32,8 +43,8 @@ def main():
 
         # decode password
         elif user_input == 2:
-            decode = decode(new_password)
-            print(f"The encoded password is {new_password}, and the original password is {decode}.\n")
+            decoded = decode(new_password)
+            print(f"The encoded password is {new_password}, and the original password is {decoded}.\n")
 
         elif user_input == 3:
             run = False
